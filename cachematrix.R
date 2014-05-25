78d09cf67680f90277f9635ab441e52eec55ef84
Prafull Kotecha <prafull.kotecha@gmail.com>
2014-05-25 15:14:14 -0400

    format comments and code

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-##This function creates a special "matrix" object
-##that can cache its inverse.
+## This function creates a special "matrix" object
+## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-        m<-NULL
-        set<-function(y){
-                x<<-y
-                m<<-NULL
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
         }
 
-        get<-function() x
-        setmatrix<-function(solve) m<<- solve
-        getmatrix<-function() m
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
         list(set=set, get=get,
              setmatrix=setmatrix,
              getmatrix=getmatrix)
 }
 
-##This function computes the inverse of the special
-##"matrix" returned by `makeCacheMatrix` above. If the inverse has
-##already been calculated (and the matrix has not changed), then
-##`cacheSolve` should retrieve the inverse from the cache.
+## This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m<-x$getmatrix()
-        if(!is.null(m)){
-                        return(m)
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                return(m)
         }
-        matrix<-x$get()
-        m<-solve(matrix, ...)
+        matrix <- x$get()
+        m <- solve(matrix, ...)
         x$setmatrix(m)
         m
 }
